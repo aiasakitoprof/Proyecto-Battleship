3d8c95c019b41d3f2ca9a39c3b44cc529671abc2
vbonnin <vbonninpalou@cifpfbmoll.eu>
2023-01-22 12:31:32 +0100

    update clase radar comentado

--- Radar.py
+++ Radar.py
@@ -1,15 +1,15 @@
-class Radar:
-    def __init__(self, width=10, height=10):
-        self.width = width
-        self.height = height
-        self.radar = [["·" for i in range(width)] for i in range(height)]
-        self.barcos_ia = []
+class Radar: # Definimos la clase radar
+    def __init__(self, width=10, height=10): # Definimos el objeto radar
+        self.width = width # Se establece el ancho del radar
+        self.height = height # Se establece el alto del radar
+        self.radar = [["·" for i in range(width)] for i in range(height)] # Creamos el radar con una matriz de  '.' con el ancho y el alto
+        self.barcos_ia = [] # Creamos una lista para almacenar los barcos en el radar
 
-    def view_radar(self):
-        print("  ", end="")
-        for i in range(self.width):
+    def view_radar(self): # Definimos la funcion que imprimira el radar
+        print("  ", end="") # Creamos un espacio separador de coordenadas
+        for i in range(self.width): #Se imprimen las coordenadas de las columnas en el radar
             print(i, end=" ")
         print()
-        for i, row in enumerate(self.radar):
+        for i, row in enumerate(self.radar):  # Se impriment las coordenadas de las filas en el radar
             print(i, end=" ")
-            print(" ".join(row))
+            print(" ".join(row))  
