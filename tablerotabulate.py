281f59d50f36e679d04909840dd52a33d878ba3c
vbonnin <vbonninpalou@cifpfbmoll.eu>
2023-01-23 10:22:44 +0100

    tablero con barcos

--- tablerotabulate.py
+++ tablerotabulate.py
@@ -32,39 +32,57 @@ class Board:
 
         table = self.board
         print(tabulate(table, header, showindex=rows, numalign="center", stralign="center", tablefmt="fancy_grid"))
+    
     def place_ship (self, ship):
-        orientation = random.choice(["horizontal", "vertical"])
-        coordinates = []
-        if orientation == "horizontal":
+        coordinates = [] 
+        unique = False 
+        while unique != True:
+            orientation = random.choice(["horizontal", "vertical"]) 
+            if orientation == "horizontal":
             #Colocamos el barco horizontalmente
             #Nos aseguramos de que el punto de inicio no resulte en el barco siendo colocado fuera del mapa
-            while True:
-                start = [random.randint(0,self.rows -1), random.randint(0, self.columns-1)]
-                while (start[1] + ship.length > self.columns):
-                    start[1] -=1
-                #Nos aseguramos de que el barco no sea colocado encima de otro barco
-                unique = True
-                for i in range(ship.length):
-                    if self.board[start[0]][start[1]+i] != "":
-                        unique = False
-                        break
-                if unique == True:
-                    break
-            for i in range(ship.length):
-                self.board[start[0]][start[1]+i] = ship.name[0]
-                coordinates.append
-
-
+                start = [random.randint(0,self.rows -1), random.randint(0, self.columns-ship.length)] 
+                
+                if start[1]+ship.length <= self.columns:
+                     #Nos aseguramos de que el barco este entre los limites del tablero y no fuera
+                    unique = True 
+                    for i in range(ship.length): 
+                        if self.board[start[0]][start[1]+i] != "":
+                            unique = False 
+                            break
+                if unique: 
+                    for i in range(ship.length):
+                        self.board[start[0]][start[1]+i] = ship.name[0] 
+                        coordinates.append([start[0],start[1]+i]) 
+            else:   
+                start = [random.randint(0,self.rows-ship.length), random.randint(0, self.columns-1)]
+                if start[0]+ship.length <= self.rows:   
+                    unique = True
+                    for i in range(ship.length):
+                        if self.board[start[0]+i][start[1]] != "":
+                            unique = False
+                            break
+                if unique:
+                    for i in range(ship.length):
+                        self.board[start[0]+i][start[1]] = ship.name[0]
+                        coordinates.append([start[0]+i,start[1]])
+        return coordinates   
 
 
 
 
 # Creamos un objeto tablero con el tamaño de 10x10
 board = Board("Test Board", [10, 10])
-# Creamos un objeto barco con el nombre "Cruiser" y longitud de 3
-cruiser = Ship("Cruiser", 3)
-
-# Lugar del barco en el tablero
-board.place_ship(cruiser)
-# Printeamos el tablero
+# Creamos un objeto barco con el nombre "Acorazado" y longitud de 5
+acorazado = Ship("Acorazado", 5)
+portaaviones = Ship("Portaaviones", 4)
+crucero = Ship("Crucero", 3)
+submarino = Ship("Submarino", 2)
+destructor = Ship("Destructor", 2)
+board.place_ship(acorazado)
+board.place_ship(portaaviones)
+board.place_ship(crucero)
+board.place_ship(submarino)
+board.place_ship(destructor)
 board.print_board()
+
